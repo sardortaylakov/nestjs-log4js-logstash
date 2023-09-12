a1f42bda90ab0936d101b3f6cacf20c5f8d808e3
sardortaylakov <staylakov@tripactions.com>
2023-09-12 00:20:58 -0700

    configure log4js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Log4jsModule } from '@nestx-log4js/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    Log4jsModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          config: {
+            appenders: {
+              logstash: {
+                type: 'log4js-logstash-tcp',
+                host: config.get<string>('LOGSTASH_HOST', 'localhost'),
+                port: config.get<number>('LOGSTASH_PORT', 5044),
+                fields: {
+                  application_name: config.get<string>('APPLICATION_NAME', 'nestjs-log4js-logstash')
+                }
+              },
+              stdout: {
+                type: 'stdout'
+              }
+            },
+            categories: {
+              default: {
+                enableCallStack: true,
+                appenders: ['logstash', 'stdout'],
+                level: 'info'
+              }
+            }
+          }
+        };
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { Log4jsLogger } from '@nestx-log4js/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(Log4jsLogger));
   await app.listen(3000);
 }
 bootstrap();
